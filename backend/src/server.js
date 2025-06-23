70b968d04ead04b2c38fd8fdf936c72cc3aaaf8b
MrFaruk0 <akdago672@gmail.com>
2025-06-23 11:50:41 +0300

    Homepage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import noteRoutes from './routes/noteRoutes.js';
-import { connnectDB } from './config/db.js';
+import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -11,11 +12,20 @@ const PORT = process.env.PORT || 5001;
 
 
 // Middleware to handle JSON requests
-app.use("/api/notes", noteRoutes);
+
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow requests from the frontend
+  methods: ['GET', 'POST', 'PUT'], // Allow only GET and POST methods and PUT for updates
+  credentials: true, // Allow cookies to be sent with requests
+}));
+
 app.use(express.json()); //PARSES JSON data in request body
 app.use(rateLimiter);
 
-connnectDB().then(() => {
+app.use("/api/notes", noteRoutes);
+
+
+connectDB().then(() => {
   app.listen(PORT, () => {
     console.log('Server is running on port', PORT);
   });
